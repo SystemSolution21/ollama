2735b8da62842a5be9f4c299f57047badf821cf3
SystemSolution21 <156997764+SystemSolution21@users.noreply.github.com>
2025-02-22 22:49:42 +0900

    modified:   fastapi_with_llm/test_api.py

--- fastapi_with_llm/test_api.py
+++ fastapi_with_llm/test_api.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Set url for uvicorn server
-url: str = "http://127.0.0.1:8000/gen_llm_res?prompt=Tell me about Python"
+url: str = "http://127.0.0.1:8000/gen_llm_res?prompt=Explain about FastAPI"
 
 # Set AIP key in headers
 headers = {"api-key": os.getenv(key="API_KEY"), "Content-Type": "application/json"}
